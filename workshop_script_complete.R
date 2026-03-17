9999a4fa88c5b50eac90789c393a29e5566c353c
Aditya Ranganath <aranganath24@gmail.com>
2026-03-16 18:11:10 -0600

    adds data and scripts

---
+++ workshop_script_complete.R
@@ -0,0 +1,75 @@
+
+# Installing and loading packages -----------------------------------------
+
+# installs workshop packages
+install.packages("tidyverse")
+install.packages("fastDummies")
+install.packages("psych")
+install.packages("stargazer")
+
+# installs workshop packages by passing a vector of package names to
+# the "install.packages()" function
+install.packages(c("tidyverse", "fastDummies", "psych", "stargazer"))
+
+# loads workshop packages into memory
+library(tidyverse)
+library(fastDummies)
+library(psych)
+library(stargazer)
+
+
+# The R Language: Fundamentals ----------------------------------------------------------
+
+# calculates 2+2
+2+2
+
+# calculates 65 to the power of 4
+65^4
+
+# calculates the sum of 24 and 4, divided by 7
+(24+4)/7
+
+# calculates 2.78 subtracted from 10.453
+10.453-2.78
+
+# assign value 5 to new object named x
+x <- 5
+
+# prints value assigned to "x"
+x
+
+# assign value 12 to new object named y
+y <- 12
+
+# prints value assigned to "y"
+y
+
+# prints the value of x + y
+x+y
+
+# creates a new object, named "xy_sum" whose value is the sum of "x" and "y"
+xy_sum <- x+y
+
+# prints value of of "xy_sum"
+xy_sum
+
+# assigns string "Boulder, CO" to object named "our_location"
+our_location <- "Boulder, CO"
+
+# prints contents assigned to "our_location" object
+our_location
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
