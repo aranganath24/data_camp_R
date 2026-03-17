4cbc2f2b2e9f886eb1027d287ca5cc6e31ac5ae1
Aditya Ranganath <aranganath24@gmail.com>
2026-03-16 23:25:48 -0600

    adds vector section

--- workshop_script_complete.R
+++ workshop_script_complete.R
@@ -59,6 +59,94 @@ our_location <- "Boulder, CO"
 # prints contents assigned to "our_location" object
 our_location
 
+# prints contents of "our_Location"; demonstrates case sensitivity
+our_Location
+
+# prints objects in memory
+ls()
+
+# deletes "our_location" object from memory
+rm(our_location)
+
+# prints objects in memory
+ls()
+
+# makes vector with values 32, 18, 41, 11
+c(32, 18, 41, 11)
+
+# assigns vector of temperatures from Asian cities to a new object # named "asia_temperatures_celsius"
+asia_temperatures_celsius<-c(32, 18, 41, 11)
+
+# prints contents of "asia_temperatures_celsius"
+asia_temperatures_celsius
+
+# defines new vector assigned to object named 
+# "university_of_colorado_locations" that contains 
+# locations of CU campuses
+university_of_colorado_locations<-c("Boulder", "Denver", "Colorado Springs")
+
+# prints contents of "university_of_colorado_locations"
+university_of_colorado_locations
+
+# assigns labels to temperatures values in 
+# "asia_temperatures_celsius"
+names(asia_temperatures_celsius)<-c("Mumbai", "Hanoi", "Singapore", "Beijing")
+
+# prints "asia_temperatures_celsius" (note labels)
+asia_temperatures_celsius
+
+# creates new vector of temperatures in Celsius of major North American cities with labels created using inline naming
+north_america_temperatures_celsius<-c("New York City"=25, "Toronto"=15, "Mexico City"=8.5, "Vancouver"=10, "Boston"=12.5)
+
+# prints contents of "north_america_temperatures_celsius"
+north_america_temperatures_celsius
+
+# Extracts the third element from the "asia_temperatures_celsius" vector
+asia_temperatures_celsius[3]
+
+# Extracts elements 1 through 3 in the "asia_temperatures_celsius" and deposits these elements in a new vector
+asia_temperatures_celsius[1:3]
+
+# tries to extract the first and third elements from "asia_temperatures_celsius" and deposit them into a new vector
+asia_temperatures_celsius[1,3]
+
+# extracts the first and third elements from "asia_temperatures_celsius" and deposits them into a new vector
+asia_temperatures_celsius[c(1,3)]
+
+# removes second element in "asia_temperatures_celsius" vector and returns a vector with the remaining values
+asia_temperatures_celsius[-2]
+
+# Extracts the third element from the "asia_temperatures_celsius" vector using its label
+asia_temperatures_celsius["Singapore"]
+
+# adds two to each element of "asia_temperatures_celsius" vector
+asia_temperatures_celsius+2
+
+# adds two to each element of "asia_temperatures_celsius" vector and assigns the changes back to the object
+asia_temperatures_celsius<-asia_temperatures_celsius+2
+
+# creates two new vectors assigned to objects "a" and "b"
+a<-c(5,6,22)
+b<-c(7,12,3)
+
+# adds vectors a and b
+a+b
+
+# creates two new vectors, "a" and "b" of unequal length
+c<-c(3,5,7)
+d<-c(6,12,3,5)
+
+# adds vectors c and d
+c+d
+
+# adds 6 as 5th element of "d"
+d<-c(d, 6)
+
+# prints updated vector d
+d
+
+# prints c+d
+c+d
 
 
 
